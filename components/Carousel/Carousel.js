b64f7c15164a95af9c9e479ea495662c2c18f1ba
Devin Warrick <dev.warr@gmail.com>
2019-05-17 11:53:06 -0500

    Bugs caught and slain. Added extra margin to bottom of cards just for better spacing at the bottom of the page.

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,72 +1,3 @@
-class Carousel {
-    constructor(carousel) {
-
-        // Defining what is what
-        this.carousel = carousel;
-        this.leftBtn = carousel.querySelector(".left-button");
-        this.rightBtn = carousel.querySelector(".right-button");
-        this.images = carousel.querySelectorAll("img");
-        this.displayImg = this.images[0];
-        this.displayIndex = 0;
-
-        // Each event listener(extra event on page load to display initial image)
-        window.addEventListener("load", () => sildeRightIn(this.displayImg));
-        this.leftBtn.addEventListener("click", () => clickLeft())
-        this.rightBtn.addEventListener("click", () => clickRight())
-    }
-    clickLeft() {
-
-        //AS LONG AS buttonPress==true
-        if(buttonPress) {
-            // Set current Img and new display image&index.
-            currentImg = this.displayImg;
-            this.displayIndex--;
-            if(this.displayIndex < 0) {
-                this.displayIndex = this.images.length - 1;
-            }
-            this.displayImg = this.images[this.displayIndex];
-
-            // Slide out and slide in, moving Left.
-            slideLeftOut(currentImg);
-            slideLeftIn(this.displayImg);
-
-            pressCheck()
-        }
-    }
-    clickRight() {
-
-        //AS LONG AS buttonPress==true
-        if(buttonPress) {
-            // Set current Img and new display image&index.
-            currentImg = this.displayImg;
-            this.displayIndex++;
-            if(this.displayIndex >= this.images.length) {
-                this.displayIndex = 0;
-            }
-            this.displayImg = this.images[this.displayIndex];
-
-            // Slide out and slide in, moving Right.
-            slideRightOut(currentImg);
-            slideRightIn(this.displayImg);
-            
-            pressCheck()
-        }
-    }
-
-}
-
-let carousel = document.querySelector(".carousel");
-new Carousel(carousel);
-
-/* If You've gotten this far, you're on your own! Although we will give you some hints:
-    1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
-    2. You will need to grab a reference to all of the images
-    3. Create a current index
-    4. Those buttons are gonna need some click handlers.
-    5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
-    6. Have fun!
-*/
-
 
 //============================ANIMATIONS============================//
 
@@ -97,24 +28,26 @@ pressCheck()
  */
 let distance;
 
-/**When each image leaves, it slides away with no easing (Power0.easeNone)
+/**When each image leaves:
+ * - It slides away with no easing (Power0.easeNone)
+ * - It slides just a little bit PAST the carousel width to keep a smoother animation
  * We also setTimeout, so after 0.5 seconds, or 500 milliseconds, 
  * The image will disappear.
  */
 function slideLeftOut(img) {
-    distance = carousel.offsetWidth;
+    distance = carousel.offsetWidth * 1.1;
     distance = distance - (distance * 2);
     TweenMax.to(img, 0.5, {x: distance, ease: Power0.easeNone})
     setTimeout(() => {
-        img.classList.remove("image--show");
+        img.style.display = "none";
         TweenMax.set(img, {x:0});
     }, 500);
 }
 function slideRightOut(img) {
-    distance = carousel.offsetWidth;
+    distance = carousel.offsetWidth * 1.1;
     TweenMax.to(img, 0.5, {x: distance, ease: Power0.easeNone})
     setTimeout(() => {
-        img.classList.remove("image--show");
+        img.style.display = "none";
         TweenMax.set(img, {x:0});
     }, 500);
 }
@@ -127,11 +60,85 @@ function slideRightOut(img) {
 function slideLeftIn(img) {
     distance = carousel.offsetWidth;
     TweenMax.from(img, 1, {x: distance, delay: 0.5})
-    setTimeout(() => img.classList.add("image--show"), 500);
+    setTimeout(() => img.style.display = "block", 500);
 }
 function slideRightIn(img) {
     distance = carousel.offsetWidth;
     distance = distance - (distance * 2);
     TweenMax.from(img, 1, {x: distance, delay: 0.5})
-    setTimeout(() => img.classList.add("image--show"), 500);
+    setTimeout(() => img.style.display = "block", 500);
 }
+
+
+
+class Carousel {
+    constructor(carousel) {
+
+        // Defining what is what
+        this.carousel = carousel;
+        this.leftBtn = carousel.querySelector(".left-button");
+        this.rightBtn = carousel.querySelector(".right-button");
+        this.images = carousel.querySelectorAll("img");
+
+        // Set initial display image and slide in
+        this.displayImg = this.images[0];
+        this.displayIndex = 0;
+        slideRightIn(this.displayImg);
+
+        // Each event listener
+        this.leftBtn.addEventListener("click", () => this.clickLeft())
+        this.rightBtn.addEventListener("click", () => this.clickRight())
+    }
+    clickLeft() {
+
+        //AS LONG AS buttonPress==true
+        if(buttonPress) {
+            // Set current Img and new display image&index.
+            let currentImg = this.displayImg;
+            this.displayIndex--;
+            if(this.displayIndex < 0) {
+                this.displayIndex = this.images.length - 1;
+            }
+            this.displayImg = this.images[this.displayIndex];
+
+            // Slide out and slide in, moving Left.
+            slideLeftOut(currentImg);
+            slideLeftIn(this.displayImg);
+
+            pressCheck()
+        }
+    }
+    clickRight() {
+
+        //AS LONG AS buttonPress==true
+        if(buttonPress) {
+            // Set current Img and new display image&index.
+            let currentImg = this.displayImg;
+            this.displayIndex++;
+            if(this.displayIndex >= this.images.length) {
+                this.displayIndex = 0;
+            }
+            this.displayImg = this.images[this.displayIndex];
+
+            // Slide out and slide in, moving Right.
+            slideRightOut(currentImg);
+            slideRightIn(this.displayImg);
+            
+            pressCheck()
+        }
+    }
+
+}
+
+let carousel = document.querySelector(".carousel");
+new Carousel(carousel);
+
+/* If You've gotten this far, you're on your own! Although we will give you some hints:
+    1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
+    2. You will need to grab a reference to all of the images
+    3. Create a current index
+    4. Those buttons are gonna need some click handlers.
+    5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
+    6. Have fun!
+*/
+
