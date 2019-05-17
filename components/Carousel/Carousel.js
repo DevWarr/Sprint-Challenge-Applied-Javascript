fe8215f1986b63d8fe172cd0e2b36f0aab93c214
Devin Warrick <dev.warr@gmail.com>
2019-05-17 11:38:02 -0500

    Carousel created. Much code was reused from previous project Tabs-Components.

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,62 @@
 class Carousel {
+    constructor(carousel) {
+
+        // Defining what is what
+        this.carousel = carousel;
+        this.leftBtn = carousel.querySelector(".left-button");
+        this.rightBtn = carousel.querySelector(".right-button");
+        this.images = carousel.querySelectorAll("img");
+        this.displayImg = this.images[0];
+        this.displayIndex = 0;
+
+        // Each event listener(extra event on page load to display initial image)
+        window.addEventListener("load", () => sildeRightIn(this.displayImg));
+        this.leftBtn.addEventListener("click", () => clickLeft())
+        this.rightBtn.addEventListener("click", () => clickRight())
+    }
+    clickLeft() {
+
+        //AS LONG AS buttonPress==true
+        if(buttonPress) {
+            // Set current Img and new display image&index.
+            currentImg = this.displayImg;
+            this.displayIndex--;
+            if(this.displayIndex < 0) {
+                this.displayIndex = this.images.length - 1;
+            }
+            this.displayImg = this.images[this.displayIndex];
+
+            // Slide out and slide in, moving Left.
+            slideLeftOut(currentImg);
+            slideLeftIn(this.displayImg);
+
+            pressCheck()
+        }
+    }
+    clickRight() {
+
+        //AS LONG AS buttonPress==true
+        if(buttonPress) {
+            // Set current Img and new display image&index.
+            currentImg = this.displayImg;
+            this.displayIndex++;
+            if(this.displayIndex >= this.images.length) {
+                this.displayIndex = 0;
+            }
+            this.displayImg = this.images[this.displayIndex];
+
+            // Slide out and slide in, moving Right.
+            slideRightOut(currentImg);
+            slideRightIn(this.displayImg);
+            
+            pressCheck()
+        }
+    }
 
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelector(".carousel");
+new Carousel(carousel);
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
@@ -12,3 +66,72 @@ let carousel = document.querySelector();
     5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
     6. Have fun!
 */
+
+
+//============================ANIMATIONS============================//
+
+/**The pressCheck() function sets buttonPress=false,
+ * then sets a timeout for buttonPress=true
+ * Inside the clickLeft() abd clickRight() functions, this is a cushion so the user
+ * cannot spam-click the left or right button and create animation glitches.
+ */
+let buttonPress;
+function pressCheck() {
+    buttonPress = false;
+    setTimeout(() => buttonPress = true, 1200);
+    console.log('booye');
+}
+pressCheck()
+
+
+
+/**This distance variable tells each animation how far to move each image.
+ * Screens can all have different sizes, right? 
+ * When each image slides away on the carousel,
+ * we need it to fully move off camera before we make it disappear.
+ * by using .offsetWidth, we can find the width of our carousel at any time.
+ * distance is the variable that holds this width number for each function.
+ * And of course,
+ * If we need a negative number, then
+ * distance = distance - (distance * 2)
+ */
+let distance;
+
+/**When each image leaves, it slides away with no easing (Power0.easeNone)
+ * We also setTimeout, so after 0.5 seconds, or 500 milliseconds, 
+ * The image will disappear.
+ */
+function slideLeftOut(img) {
+    distance = carousel.offsetWidth;
+    distance = distance - (distance * 2);
+    TweenMax.to(img, 0.5, {x: distance, ease: Power0.easeNone})
+    setTimeout(() => {
+        img.classList.remove("image--show");
+        TweenMax.set(img, {x:0});
+    }, 500);
+}
+function slideRightOut(img) {
+    distance = carousel.offsetWidth;
+    TweenMax.to(img, 0.5, {x: distance, ease: Power0.easeNone})
+    setTimeout(() => {
+        img.classList.remove("image--show");
+        TweenMax.set(img, {x:0});
+    }, 500);
+}
+
+
+/**Here, there's a delay on both the animation and the image appearance.
+ * This gives the leaving image enough time to fully disappear so that 
+ * our new image doesn't glitch as it slides in.
+ */
+function slideLeftIn(img) {
+    distance = carousel.offsetWidth;
+    TweenMax.from(img, 1, {x: distance, delay: 0.5})
+    setTimeout(() => img.classList.add("image--show"), 500);
+}
+function slideRightIn(img) {
+    distance = carousel.offsetWidth;
+    distance = distance - (distance * 2);
+    TweenMax.from(img, 1, {x: distance, delay: 0.5})
+    setTimeout(() => img.classList.add("image--show"), 500);
+}
